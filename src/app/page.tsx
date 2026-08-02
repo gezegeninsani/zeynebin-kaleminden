ebfe5a91014e2084ccc182bdf4825f8b0aa21f5d
gezegeninsani <jannntr@gmail.com>
2026-08-02 23:05:48 +0300

    Update main page with layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
-import { BlogFeed } from '@/components/BlogFeed';
+"use client";
+
+import React from "react";
+import Sidebar from "../components/Sidebar";
+import PostView from "../components/PostView";
 
 export default function HomePage() {
-  return <BlogFeed />;
+  return (
+    <div className="flex min-h-screen bg-pink-50">
+      <Sidebar />
+      <PostView />
+    </div>
+  );
 }
